cd2e393f2ff72760fdd0ec85ba50923199211233
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-05-18 18:32:02 +0800

    membuat class induk RekeningBank  dengan no, saldo, dan fungsi virtual potong admin

--- tugas.cpp
+++ tugas.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 using namespace std;
 
+class RekeningBank {
+protected:
+    string noRek;
+    double saldo;
+
+public:
+    RekeningBank(string no, double saldoAwal) {
+        noRek = no;
+        saldo = saldoAwal;
+    }
+
+    virtual void potongAdmin() = 0; 
+
+    void tampilkanInfo() {
+        cout << "No Rekening: " << noRek << " | Saldo: Rp " << saldo << endl;
+    }
+};
+
