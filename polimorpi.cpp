bb4b7bb4cd5af19b3c69dded0a2ec560ba983dd7
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-05-18 15:23:53 +0800

    menambahkan program utama

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -23,3 +23,14 @@ public:
     }
 };
 
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
+}
