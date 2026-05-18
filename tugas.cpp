02acc47b6821aea6d2fc1f54eccb59f455600558
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-05-18 18:33:52 +0800

    menambahkan class RekeningPremium

--- tugas.cpp
+++ tugas.cpp
@@ -39,3 +39,20 @@ public:
     }
 };
 
+class RekeningPremium : public RekeningBank {
+public:
+    RekeningPremium(string no, double saldoAwal) : RekeningBank(no, saldoAwal) {}
+
+    void potongAdmin() override {
+        double admin;
+        if (saldo > 10000000) {
+            admin = 0;
+            cout << "Rekening Premium: Saldo di atas Rp 10.000.000 (Bebas biaya admin)." << endl;
+        } else {
+            admin = 50000;
+            saldo -= admin;
+            cout << "Rekening Premium: Saldo <= Rp 10.000.000. Dipotong biaya admin Rp " << admin << endl;
+        }
+    }
+};
+
